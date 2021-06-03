6ca9e7410faf2b537b9fa0e3c1efb4656bf5345c
absgeorge <admin@admin.com>
2021-06-03 14:06:15 +0000

    s1-s4

--- servicefour/tests/test_server.py
+++ servicefour/tests/test_server.py
@@ -1,17 +1,33 @@
-from flask import url_for
+from flask.helpers import url_for
 from flask_testing import TestCase
-import requests_mock 
+import requests
+import json
 
-from app import app
+from servicefour.app import app, db, WinningNumbers
 
 class TestBase(TestCase):
     def create_app(self):
         return app
 
+class TestBase(TestCase):
+    def create_app(self):
+        app.config.update(
+                SQLALCHEMY_DATABASE_URI="sqlite:///data.db",
+                DEBUG=True,        
+            )
+        return app
+
+    def setUp(self):
+        db.create_all()
+        
+
+    def tearDown(self):
+        db.drop_all()
+
 class TestHome(TestBase):
     def test_home(self):
+        info = dict(number="12345", alphabet="ab", entry="12345ab")
+        response = self.client.post(url_for('home'), json=info)       
         
-        response = self.client.post(url_for('/'))
-        self.assertEqual(response.data.decode(), entry = ["entry"],
-        lotterynumber = ["number"],
-        lotteryalpha = ["alphabet"])
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data.decode(), "You have won £5 million")
