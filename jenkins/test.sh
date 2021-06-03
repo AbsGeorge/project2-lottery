61b05c99a553088acc88259885d0c7e57caa618c
absgeorge <admin@admin.com>
2021-06-03 16:54:01 +0000

    compilingjfile

---
+++ jenkins/test.sh
@@ -0,0 +1,16 @@
+#install apt dependencies 
+sudo apt-get update 
+sudo apt-get install python3 python3-pip python3-venv -y
+
+#create and source virtual environment 
+python3 -m venv venv 
+source venv/bin/activate 
+
+#install pip requirements 
+pip3 install -r requirements.txt
+
+#run pytest
+python3 -m pytest serviceone --cov=application  --cov-repot=xml --junitxml=junit/test-results.xml
+python3 -m pytest servicetwo --cov=application  --cov-repot=xml --junitxml=junit/test-results.xml
+python3 -m pytest servicethree --cov=application  --cov-repot=xml --junitxml=junit/test-results.xml
+python3 -m pytest servicefour --cov=application  --cov-repot=xml --junitxml=junit/test-results.xml
