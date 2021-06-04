0a6df8e1b9f66c3d2d2ebf200766bc7fa6375141
absgeorge <admin@admin.com>
2021-06-04 08:00:29 +0000

    ansible

--- jenkins/setup-docker.sh
+++ jenkins/setup-docker.sh
@@ -4,6 +4,9 @@
 # Install docker 
 curl https://get.docker.com | sudo bash
 
+# Add jenkins to docker group 
+sudo usermod aG docker jenkins 
+
 #log into Docker 
 
  | docker login -u $DOCKER_LOGIN_USR -p-stdin $DOCKER_LOGIN_PSW
