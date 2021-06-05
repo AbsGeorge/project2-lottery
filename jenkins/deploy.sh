7aca4ab772c1bce04a2b049131ab3f0f786b9072
Your Name <you@example.com>
2021-06-05 19:02:21 +0000

    renaming servers

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -2,5 +2,5 @@ scp docker-compose.yaml swarm-master:
 ssh swarm-master << EOF 
 export SECRET_KEY=${SECRET_KEY}
 export DATABASE_URI=${SQLALCHEMY_DATABASE_URI}
-docker stak deploy --compose-file docker-compose.yaml app
-dcoker exec loterry_numbers_server python3 create.py 
+docker stack deploy --compose-file docker-compose.yaml app
+docker exec lottery_numbers_server python3 create.py 
