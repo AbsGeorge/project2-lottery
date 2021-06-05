71c193a574394edadc32c36ad71448df48a0e312
Your Name <you@example.com>
2021-06-05 19:06:12 +0000

    removed app from dep.sh

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -2,5 +2,5 @@ scp docker-compose.yaml swarm-master:
 ssh swarm-master << EOF 
 export SECRET_KEY=${SECRET_KEY}
 export DATABASE_URI=${SQLALCHEMY_DATABASE_URI}
-docker stack deploy --compose-file docker-compose.yaml app
+docker stack deploy --compose-file docker-compose.yaml 
 docker exec lottery_numbers_server python3 create.py 
