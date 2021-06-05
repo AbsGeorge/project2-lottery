f8f03b1daea2aa9d39d28ddd785f68268876bff6
Your Name <you@example.com>
2021-06-05 18:54:15 +0000

    changes to data_url

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -1,6 +1,6 @@
 scp docker-compose.yaml swarm-master:
 ssh swarm-master << EOF 
 export SECRET_KEY=${SECRET_KEY}
-export DATABASE_URI=${DATABASE_URI}
+export DATABASE_URI=${SQLALCHEMY_DATABASE_URI}
 docker stak deploy --compose-file docker-compose.yaml app
 dcoker exec loterry_numbers_server python3 create.py 
