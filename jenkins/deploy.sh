b82efc818b6eecef88c0c00a3bb3dbea7401e019
Your Name <you@example.com>
2021-06-05 19:38:22 +0000

    quotes on database

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -1,6 +1,6 @@
 scp docker-compose.yaml swarm-master:
 ssh swarm-master << EOF 
 export SECRET_KEY=${SECRET_KEY}
-export DATABASE_URI=${DATABASE_URI}
+export DATABASE_URI="${DATABASE_URI}"
 docker stack deploy --compose-file docker-compose.yaml lottery_numbers_stack
 EOF
